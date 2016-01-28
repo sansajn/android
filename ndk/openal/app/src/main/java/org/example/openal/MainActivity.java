59a8cf0f3bff08cec423347d372a62bfc6b579d7
Adam Hlavatovic <adam.hlavatovic@gmail.com>
2016-01-29 00:14:46 +0100

    ndk: openal sample program

---
+++ ndk/openal/app/src/main/java/org/example/openal/MainActivity.java
@@ -0,0 +1,36 @@
+package org.example.openal;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends ActionBarActivity {
+
+	@Override protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		play_sound();
+		_update_timer = new Timer("sound_update_timer");
+		_update_timer.scheduleAtFixedRate(new TimerTask() {
+			public void run() {
+				update();
+			}
+		}, 0, 50);  // update every 50ms
+	}
+
+	@Override protected void onDestroy() {
+		super.onDestroy();
+		free();
+	}
+
+	private native void play_sound();
+	private native void update();
+	private native void free();
+
+	private Timer _update_timer;
+
+	static {
+		System.loadLibrary("sound");  // loads libsound
+	}
+}
