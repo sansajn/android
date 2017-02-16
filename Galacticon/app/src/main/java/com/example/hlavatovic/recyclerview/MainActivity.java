018838a079e142b153f1aeff62bc3f360bf238a7
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2017-02-16 21:29:32 +0100

    Galacticon: remove by swipe

--- Galacticon/app/src/main/java/com/example/hlavatovic/recyclerview/MainActivity.java
+++ Galacticon/app/src/main/java/com/example/hlavatovic/recyclerview/MainActivity.java
@@ -2,8 +2,13 @@ package com.example.hlavatovic.recyclerview;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -18,12 +23,14 @@ public class MainActivity extends Activity implements ImageRequester.ImageReques
 		_photos = new ArrayList<>();
 		_imageRequester = new ImageRequester(this);
 		_photoAdapter = new PhotoAdapter(_photos);
-		_layoutManager = new LinearLayoutManager(this);
+		_linearLayout = new LinearLayoutManager(this);
+		_gridLayout = new GridLayoutManager(this, 2);
 
 		_recyclerView = (RecyclerView)findViewById(R.id.recyclerView);
 		_recyclerView.setAdapter(_photoAdapter);
-		_recyclerView.setLayoutManager(_layoutManager);
+		_recyclerView.setLayoutManager(_linearLayout);
 		setRecyclerViewScrollListener();
+		setRecyclerViewItemTouchListener();
 	}
 
 	@Override
@@ -33,8 +40,27 @@ public class MainActivity extends Activity implements ImageRequester.ImageReques
 			requestPhoto();
 	}
 
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (item.getItemId() == R.id.action_change_recycler_manager) {
+			changeLayoutManager();
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.menu_main, menu);
+		return true;
+	}
+
 	private int getLastVisibleItemPosition() {
-		return _layoutManager.findLastVisibleItemPosition();
+		if (_recyclerView.getLayoutManager().equals(_linearLayout))
+			return _linearLayout.findLastVisibleItemPosition();
+		else
+			return _gridLayout.findLastVisibleItemPosition();
 	}
 
 	@Override
@@ -68,9 +94,40 @@ public class MainActivity extends Activity implements ImageRequester.ImageReques
 		});
 	}
 
+	private void setRecyclerViewItemTouchListener() {
+		ItemTouchHelper.SimpleCallback itemTouchCallback =
+			new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT|ItemTouchHelper.RIGHT) {
+				@Override
+				public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder viewHolder1) {
+					return false;
+				}
+
+				@Override
+				public void onSwiped(RecyclerView.ViewHolder viewHolder, int swipeDir) {
+					int position = viewHolder.getAdapterPosition();
+					_photos.remove(position);
+					_recyclerView.getAdapter().notifyItemRemoved(position);
+				}
+			};
+
+		ItemTouchHelper itemTouchHelper = new ItemTouchHelper(itemTouchCallback);
+		itemTouchHelper.attachToRecyclerView(_recyclerView);
+	}
+
+	private void changeLayoutManager() {
+		if (_recyclerView.getLayoutManager().equals(_linearLayout)) {
+			_recyclerView.setLayoutManager(_gridLayout);
+			if (_photos.size() == 1)
+				requestPhoto();
+		}
+		else
+			_recyclerView.setLayoutManager(_linearLayout);
+	}
+
 	private ArrayList<Photo> _photos;
 	private ImageRequester _imageRequester;
 	private PhotoAdapter _photoAdapter;
-	private LinearLayoutManager _layoutManager;
+	private LinearLayoutManager _linearLayout;
+	private GridLayoutManager _gridLayout;
 	private RecyclerView _recyclerView;
 }
