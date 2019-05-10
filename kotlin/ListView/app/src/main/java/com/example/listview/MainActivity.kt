885549d6f42b72a5e62151af507c660d5fc17671
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2019-05-10 10:00:42 +0200

    kotlin: ListView sample

---
+++ kotlin/ListView/app/src/main/java/com/example/listview/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.listview
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
+
+		var items = listOf("one", "two", "three")
+		listview.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, items)
+	}
+}
