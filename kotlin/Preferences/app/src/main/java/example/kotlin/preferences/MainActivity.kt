b750c76a991f0b0e1630fa1f6d90962ee73318e5
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2019-07-16 15:11:31 +0200

    kotlin: how to access preference element

--- kotlin/Preferences/app/src/main/java/example/kotlin/preferences/MainActivity.kt
+++ kotlin/Preferences/app/src/main/java/example/kotlin/preferences/MainActivity.kt
@@ -3,6 +3,7 @@ package example.kotlin.preferences
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.preference.PreferenceManager
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,5 +20,7 @@ class MainActivity : AppCompatActivity() {
 		super.onResume()
 
 		val sharedPref = PreferenceManager.getDefaultSharedPreferences(this)
+		val zipCode = sharedPref.getString(getString(R.string.pref_key_zipcode), "")
+		// do something with zipCode ...
 	}
 }
