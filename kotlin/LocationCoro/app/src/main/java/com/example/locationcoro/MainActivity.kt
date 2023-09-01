bc88de6718cb1c05f7e898a21e56416c9844d23f
Hlavatovic Adam <adam.hlavatovic@siemens.com>
2023-09-01 16:48:12 +0200

    partial work

--- kotlin/LocationCoro/app/src/main/java/com/example/locationcoro/MainActivity.kt
+++ kotlin/LocationCoro/app/src/main/java/com/example/locationcoro/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.locationcoro
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,7 +11,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
 import com.example.locationcoro.ui.theme.LocationCoroTheme
+import kotlinx.coroutines.*
 
 class MainActivity : ComponentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,6 +29,18 @@ class MainActivity : ComponentActivity() {
 				}
 			}
 		}
+
+		lifecycleScope.launch {
+			performBackgroundTask()
+		}
+	}
+
+	private fun performBackgroundTask() {
+		Log.d(TAG, "hello from coroutine!")
+	}
+
+	companion object {
+		const val TAG = "LocationCoro"
 	}
 }
 
